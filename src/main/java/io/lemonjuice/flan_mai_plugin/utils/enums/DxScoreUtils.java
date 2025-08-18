7803901651a950cb627813fe2538144cefec6924
lemon_juice_1 <1582017385@qq.com>
2025-08-18 17:14:13 +0800

    修正dx分星数判定

--- src/main/java/io/lemonjuice/flan_mai_plugin/utils/enums/DxScoreUtils.java
+++ src/main/java/io/lemonjuice/flan_mai_plugin/utils/enums/DxScoreUtils.java
@@ -3,15 +3,15 @@ package io.lemonjuice.flan_mai_plugin.utils.enums;
 public class DxScoreUtils {
     public static int getStarNum(int score, int totalScore) {
         int rate = score * 100 / totalScore;
-        if(rate <= 85)
+        if(rate < 85)
             return 0;
-        else if(rate <= 90)
+        else if(rate < 90)
             return 1;
-        else if(rate <= 93)
+        else if(rate < 93)
             return 2;
-        else if(rate <= 95)
+        else if(rate < 95)
             return 3;
-        else if(rate <= 97)
+        else if(rate < 97)
             return 4;
         else
             return 5;

499f37680fa8adf5362ff74c943e6ab547642d18
lemon_juice_1 <1582017385@qq.com>
2025-08-21 15:19:50 +0800

    将配置文件内容改为使用Supplier提供

--- src/main/java/io/lemonjuice/flan_mai_plugin/refence/ConfigRefs.java
+++ src/main/java/io/lemonjuice/flan_mai_plugin/refence/ConfigRefs.java
@@ -4,13 +4,16 @@ import lombok.extern.log4j.Log4j2;
 
 import java.io.*;
 import java.util.Properties;
+import java.util.function.Supplier;
 
 @Log4j2
 public class ConfigRefs {
-    public static String BOT_NAME;
-    public static String DIVING_FISH_TOKEN;
+    private static final Properties properties = new Properties();
 
-    public static void init() {
+    public static final Supplier<String> BOT_NAME = () -> properties.getProperty("bot.name");
+    public static final Supplier<String> DIVING_FISH_TOKEN = () -> properties.getProperty("diving_fish.dev_token");
+
+    public static synchronized void init() {
         File cfgFile = new File("./config/mai_plugin.properties");
         if(!cfgFile.getParentFile().exists()) {
             cfgFile.getParentFile().mkdirs();
@@ -19,12 +22,7 @@ public class ConfigRefs {
             releaseConfigFile();
         }
         try (FileInputStream input = new FileInputStream(cfgFile)) {
-            Properties properties = new Properties();
             properties.load(input);
-
-            BOT_NAME = properties.getProperty("bot.name");
-            DIVING_FISH_TOKEN = properties.getProperty("diving_fish.dev_token");
-
         } catch (IOException e) {
             log.error("加载舞萌插件配置文件失败！", e);
         }

89b54fc87b7329d06b99e7fabc05a8d8c164d308
lemon_juice_1 <1582017385@qq.com>
2025-09-18 19:43:05 +0800

    使用新的EventBus实现

--- src/main/java/io/lemonjuice/flan_mai_plugin/FlandreMaiPlugin.java
+++ src/main/java/io/lemonjuice/flan_mai_plugin/FlandreMaiPlugin.java
@@ -1,9 +1,9 @@
 package io.lemonjuice.flan_mai_plugin;
 
-import com.google.common.eventbus.Subscribe;
 import io.lemonjuice.flan_mai_plugin.refence.ConfigRefs;
 import io.lemonjuice.flan_mai_plugin.utils.SongManager;
 import io.lemonjuice.flandre_bot_framework.event.annotation.EventSubscriber;
+import io.lemonjuice.flandre_bot_framework.event.annotation.SubscribeEvent;
 import io.lemonjuice.flandre_bot_framework.event.meta.PluginRegisterEvent;
 import io.lemonjuice.flandre_bot_framework.plugins.BotPlugin;
 
@@ -20,7 +20,7 @@ public class FlandreMaiPlugin implements BotPlugin {
         return "Flandre MaiMai Plugin";
     }
 
-    @Subscribe
+    @SubscribeEvent
     public void registerPlugin(PluginRegisterEvent event) {
         event.register(this);
     }
